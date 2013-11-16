36d2e3134ddda6503c5acb8642fd356eec67e824
lion0738 <lion0738@naver.com>
2013-11-16 19:24:14 +0900

    Add more audio files
    Change-Id: I10d5bcd0c2a46e16438c3a46d94d8ac596a71e1a

--- config/cm_audio.mk
+++ config/cm_audio.mk
@@ -10,7 +10,8 @@ UI_PATH := vendor/cm/prebuilt/common/media/audio/ui
 # Alarms
 PRODUCT_COPY_FILES += \
 	$(ALARM_PATH)/CyanAlarm.ogg:system/media/audio/alarms/CyanAlarm.ogg \
-	$(ALARM_PATH)/NuclearLaunch.ogg:system/media/audio/alarms/NuclearLaunch.ogg
+	$(ALARM_PATH)/NuclearLaunch.ogg:system/media/audio/alarms/NuclearLaunch.ogg \
+	$(ALARM_PATH)/Walk_in_the_forest.ogg:system/media/audio/alarms/Walk_in_the_forest.ogg \
 
 # Notifications
 PRODUCT_COPY_FILES += \
@@ -21,7 +22,10 @@ PRODUCT_COPY_FILES += \
 	$(NOTIFICATION_PATH)/Naughty.ogg:system/media/audio/notifications/Naughty.ogg \
 	$(NOTIFICATION_PATH)/Pong.ogg:system/media/audio/notifications/Pong.ogg \
 	$(NOTIFICATION_PATH)/Rang.ogg:system/media/audio/notifications/Rang.ogg \
-	$(NOTIFICATION_PATH)/Stone.ogg:system/media/audio/notifications/Stone.ogg
+	$(NOTIFICATION_PATH)/Stone.ogg:system/media/audio/notifications/Stone.ogg \
+	$(NOTIFICATION_PATH)/Sherbet.ogg:system/media/audio/notifications/Sherbet.ogg \
+	$(NOTIFICATION_PATH)/S_Whistle.ogg:system/media/audio/notifications/S_Whistle.ogg \
+	$(NOTIFICATION_PATH)/Whistle.ogg:system/media/audio/notifications/Whistle.ogg
 
 # Ringtones
 PRODUCT_COPY_FILES += \
@@ -30,6 +34,6 @@ PRODUCT_COPY_FILES += \
 	$(RINGTONE_PATH)/Highscore.ogg:system/media/audio/ringtones/Highscore.ogg \
 	$(RINGTONE_PATH)/Lyon.ogg:system/media/audio/ringtones/Lyon.ogg \
 	$(RINGTONE_PATH)/Rockin.ogg:system/media/audio/ringtones/Rockin.ogg \
-	$(RINGTONE_PATH)/Sheep.mp3:system/media/audio/ringtones/Sheep.mp3
-
+	$(RINGTONE_PATH)/Sheep.mp3:system/media/audio/ringtones/Sheep.mp3 \
+	$(RINGTONE_PATH)/S_Over_the_horizon.ogg:system/media/audio/ringtones/S_Over_the_horizon.ogg
 
